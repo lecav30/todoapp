e96ffc551ae100042d5aa703ba57664c10e835ad
lecav30 <lecav30@outlook.com>
2025-08-31 19:10:20 -0500

    feat: update reducers

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,7 +1,15 @@
 import sidebarReducer from "@feature/sidebar/sidebar.slice";
+import projectReducer from "@feature/project/project.slice";
+import groupReducer from "@feature/group/group.slice";
+import taskReducer from "@feature/task/task.slice";
+import authReducer from "@feature/auth/auth.slice";
 
 const rootReducer = {
   sidebar: sidebarReducer,
+  project: projectReducer,
+  group: groupReducer,
+  task: taskReducer,
+  auth: authReducer,
 };
 
 export default rootReducer;
