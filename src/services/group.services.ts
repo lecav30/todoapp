33c8367c0c8bf06905b44e87e309143d4f370059
lecav30 <lecav30@outlook.com>
2025-08-31 20:17:01 -0500

    fix: group services

--- src/services/group.services.ts
+++ src/services/group.services.ts
@@ -1,11 +1,11 @@
 import { requests } from "@core/axiosAgent";
 
-const createGroup = (project: any) => requests.post("/project/create", project);
+const createGroup = (group: any) => requests.post("/group/create", group);
 
 const getGroupsByProjectId = (projectId: number) =>
-  requests.get(`/project/getByProject/${projectId}`);
+  requests.get(`/group/getByProject/${projectId}`);
 
-const getGroupById = (id: number) => requests.get(`/project/${id}`);
+const getGroupById = (id: number) => requests.get(`/group/${id}`);
 
 export default {
   createGroup,
