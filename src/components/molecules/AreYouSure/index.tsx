7279fd34437365cbddfd4de01a3f79cdb613a71e
lecav30 <lecav30@outlook.com>
2025-09-20 12:24:29 -0500

    feat: are you sure dialog

---
+++ src/components/molecules/AreYouSure/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+import TodoDialog from "@components/atoms/Dialog";
+import TodoButton from "@components/atoms/TodoButton";
+import { useAppDispatch, useAppSelector } from "@core/store";
+import { resetAreYouSureDialog } from "@feature/common/common.thunk";
+import { deleteTask } from "@feature/task/task.thunk";
+
+const AreYouSure = () => {
+  const dispatch = useAppDispatch();
+
+  const { areYouSureDialog } = useAppSelector((state) => state.common);
+
+  const closeDialog = () => {
+    dispatch(resetAreYouSureDialog());
+  };
+
+  const confirmAction = () => {
+    switch (areYouSureDialog.actionType) {
+      case "DELETE_TASK":
+        dispatch(
+          deleteTask({
+            taskId: areYouSureDialog.taskId!,
+            groupId: areYouSureDialog.groupId!,
+          }),
+        );
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <TodoDialog isOpen={areYouSureDialog.open} onClose={closeDialog} title={""}>
+      <p>{areYouSureDialog.message}</p>
+      <div className="flex gap-5 justify-center">
+        <TodoButton customclass="cursor-pointer" onClick={closeDialog}>
+          Cancel
+        </TodoButton>
+        <TodoButton customclass="cursor-pointer" onClick={confirmAction}>
+          Confirm
+        </TodoButton>
+      </div>
+    </TodoDialog>
+  );
+};
+
+export default AreYouSure;
