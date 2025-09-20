ddc73460ccb3bbda2857b6a1a5228123e7d125d7
lecav30 <lecav30@outlook.com>
2025-09-20 12:25:20 -0500

    feat: modal context provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import Login from "@views/Auth/Login";
 import ProtectedLayout from "./layouts/ProtectedLayout";
 import PublicLayout from "./layouts/PublicLayout";
 import { AuthProvider } from "@context/AuthContext";
+import { ModalProvider } from "@context/ModalContext";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <AuthProvider>
-      <Routes>
-        <Route element={<ProtectedLayout />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-        <Route element={<PublicLayout />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
-      </Routes>
+      <ModalProvider>
+        <Routes>
+          <Route element={<ProtectedLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route element={<PublicLayout />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </ModalProvider>
     </AuthProvider>
   );
 }
